c6bab8db52a345fb784b82fafd31f9da4a4b86ec
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-25 10:08:05 +0530

    [Rohit]Add: // add all files

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        private int count;
+
+        public FibonacciSeries(int count)
+        {
+            this.count = count;
+        }
+
+        public void GenerateSeries()
+        {
+            int first = 0;
+            int second = 1;
+            Console.WriteLine("Fibonacci series:");
+            Console.Write(first + " " + second + " ");
+
+            for (int i = 2; i < count; i++)
+            {
+                int next = first + second;
+                Console.Write(next + " ");
+
+                first = second;
+                second = next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the count of terms in the Fibonacci series:");
+            int count = int.Parse(Console.ReadLine());
+
+            if (count <= 0)
+            {
+                Console.WriteLine("Invalid input. Count must be a positive integer.");
+                return;
+            }
+
+            FibonacciSeries fibonacciSeries = new FibonacciSeries(count);
+            fibonacciSeries.GenerateSeries();
+        }
+    }
+}
